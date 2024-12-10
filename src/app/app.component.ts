1d079e2883682ac7878efadd96aadc64d95b9661
Janpreet <95113286+janpreett@users.noreply.github.com>
2024-12-10 00:20:51 -0500

    created basic structure of weather widget with hardcoded data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'weather-app';
+  temperature = 111;
+  coldImageUrl = 'assets/cold.jpg';
+  hotImageUrl = 'assets/hot.jpg';
+  imageUrl!: string;
+
+  weatherState = 'Cloudy';
+  minTemp = 5;
+  maxTemp = 15;
+  feelsLike = 8;
+  humidity = 80;
+  windSpeed = 5;
+
+  constructor(){
+    this.updateImage();
+  }
+
+  updateImage(): void {
+    if (this.temperature < 15) {
+      this.imageUrl = this.coldImageUrl;
+    } else {
+      this.imageUrl = this.hotImageUrl;
+    }
+  }
 }
